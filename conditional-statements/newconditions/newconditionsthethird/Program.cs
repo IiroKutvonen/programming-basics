f229423eae23932de52ad3a2072641b84343a881
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2017-10-24 11:03:45 +0300

    Updatelius

--- conditional-statements/newconditions/newconditionsthethird/Program.cs
+++ conditional-statements/newconditions/newconditionsthethird/Program.cs
@@ -9,13 +9,23 @@ namespace newconditionsthethird
             Console.WriteLine("Ohjelma valitsee 20 satunnaista numeroa väliltä 0-50 ja tulostaa ne neljässä rivissä.");
             Random rnd = new Random();
 
-            for (int i = 1; i < 20; i++)
-            {
-                rnd.Next(0,51);
-            }
+            //for (int i = 0; i < 4; i++)
+            //  {
+            //   Console.Write($"Rivi {i}: ");
+            //    for (int j = 0; j < 5; j++)
+            //    {
+            //      Console.Write($"{rnd.Next(0, 51)}, ");
+            //  }
+            //    Console.WriteLine();
+
+            Console.WriteLine($"Rivi 1: {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}");
+            Console.WriteLine($"Rivi 2: {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}");
+            Console.WriteLine($"Rivi 3: {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}");
+            Console.WriteLine($"Rivi 4: {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}, {rnd.Next(0, 51)}");
+
+            Console.ReadKey();
 
 
-        
 
 
         }
