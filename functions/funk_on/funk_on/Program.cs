263eeea45fde47a0d280ff50d67e89a00161f7c4
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2017-10-25 13:59:27 +0300

    FUNK ON!

---
+++ functions/funk_on/funk_on/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace funk_on
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eka;
+            Console.WriteLine("Syötä ensimmäinen luku.");
+            int.TryParse(Console.ReadLine(), out eka);
+
+            int toka;
+            Console.WriteLine("Syötä toinen luku.");
+            int.TryParse(Console.ReadLine(), out toka);
+
+            Console.WriteLine($"Pienempi luku on: {vertailu(toka, eka)}");
+
+            Console.ReadKey();
+        }
+        static int vertailu(int eka, int toka)
+        {
+            int vertaa;
+
+            if (eka > toka)
+            {
+                vertaa = toka;
+            }
+            else if (toka > eka)
+            {
+                vertaa = eka;
+            }
+
+            return vertaa;
+        }
+    }
+}
