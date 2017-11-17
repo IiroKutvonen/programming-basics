ac4a7df1808d5e61e9e44012936490cefedbc2ad
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2017-11-17 12:34:00 +0200

    POSH

---
+++ functions/funk_on/funk_on2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace funk_on2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kysyy lukua väliltä 1-10");
+
+            int lowerBound;
+            int upperBound;
+            int luku;
+
+            Console.WriteLine("Syötä luku.");
+            int.TryParse(Console.ReadLine(), out luku);
+
+
+            static int vertailu(int luku)
+            {
+                if (luku < 1 && luku > 10)
+                {
+                    Console.WriteLine("Luku ei ole väliltä 1-10");
+                }
+                else
+                {
+                    Console.WriteLine("Luku on väliltä 1-10");
+                }
+            }
+            retNumber = numberFromRange(lowerBound, upperBound);
+        }
+    }
+}
