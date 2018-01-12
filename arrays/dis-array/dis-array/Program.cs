2cf6eac7d7a2a3e3a59945d6529306498a189c18
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2018-01-12 08:51:51 +0200

    ARRAY

---
+++ arrays/dis-array/dis-array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace dis_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int sum = 0;
+            int[] iT = new int[100];
+            Random rnd = new Random();
+
+            while (i < 99)
+            {
+                iT[i] = rnd.Next(1, 51);
+                sum += iT[i];
+                i++;
+            }
+
+            Console.WriteLine($"Keskiarvo on {sum / 100}");
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ arrays/dis-array/dis-array2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace dis_array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int[] iT = new int[7];
+            Random rnd = new Random();
+
+            while (i < 6)
+            {
+                iT[i] = rnd.Next(1, 40);
+                i++;
+            }
+
+
+        }
+    }
+}
